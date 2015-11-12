42d554844cc302d360c9e61adcafe995b34a2410
afaly <afathygo@gmail.com>
2015-11-12 16:07:49 +0300

    toString()

--- PrincetonII/src/week_01/Edge.java
+++ PrincetonII/src/week_01/Edge.java
@@ -2,59 +2,64 @@ package week_01;
 
 public class Edge {
 
-	private int src, dst;
-	private double val;
+    private int src, dst;
+    private double val;
 
-	public Edge(int src, int dst) {
-		this.dst = dst;
-		this.val = 0.0;
-	}
+    public Edge(int src, int dst) {
+        this.dst = dst;
+        this.val = 0.0;
+    }
 
-	public Edge(int src, int dst, double val) {
-		this.dst = dst;
-		this.val = val;
-	}
+    public Edge(int src, int dst, double val) {
+        this.dst = dst;
+        this.val = val;
+    }
 
-	public int dst() {
-		return dst;
-	}
+    public int dst() {
+        return dst;
+    }
 
-	public int src() {
-		return src;
-	}
+    public int src() {
+        return src;
+    }
 
-	public int either() {
-		return src;
-	}
+    public int either() {
+        return src;
+    }
 
-	public int other(int v) {
-		return v == src ? dst : src;
-	}
+    public int other(int v) {
+        return v == src ? dst : src;
+    }
 
-	public double val() {
-		return val;
-	}
+    public double val() {
+        return val;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + dst;
-		long temp;
-		temp = Double.doubleToLongBits(val);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + dst;
+        long temp;
+        temp = Double.doubleToLongBits(val);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) return true;
-		if (obj == null) return false;
-		if (!(obj instanceof Edge)) return false;
-		Edge other = (Edge) obj;
-		if (dst != other.dst) return false;
-		if (Double.doubleToLongBits(val) != Double.doubleToLongBits(other.val)) return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (!(obj instanceof Edge)) return false;
+        Edge other = (Edge) obj;
+        if (dst != other.dst) return false;
+        if (Double.doubleToLongBits(val) != Double.doubleToLongBits(other.val)) return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Edge [" + src + " -> " + dst + "= " + val + "]";
+    }
 
 }
